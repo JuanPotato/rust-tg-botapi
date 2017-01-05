d800c653a7df8e0f9d5f2b23a73a07e679c0562f
Juan Potato <hasantiny@gmail.com>
2017-01-04 23:46:48 -0500

    Forgot about build.rs

--- build.rs
+++ build.rs
@@ -9,9 +9,5 @@ fn main() {
     let src = Path::new("src/types.in.rs");
     let dst = Path::new(&out_dir).join("types.rs");
 
-    let src2 = Path::new("src/method_return_types.in.rs");
-    let dst2 = Path::new(&out_dir).join("method_return_types.rs");
-
     serde_codegen::expand(&src, &dst).unwrap();
-    serde_codegen::expand(&src2, &dst2).unwrap();
 }
