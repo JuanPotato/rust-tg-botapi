ea5c96e1ac6ac52a795b37450fd03d21dad04c1e
Juan Potato <hasantiny@gmail.com>
2017-01-05 23:55:38 -0500

    Improved url formatting, started adding more methods
    Requests now use Post. Improved 'test' code

--- src/args.rs
+++ src/args.rs
@@ -11,295 +11,254 @@ pub struct GetUpdates<'a> {
     pub allowed_updates: Option<Vec<&'a str>>,
 }
 
-impl<'a> GetUpdates<'a> {
-    pub fn new() -> GetUpdates<'a> {
-        GetUpdates {
-            offset: None,
-            limit: None,
-            timeout: None,
-            allowed_updates: None,
-        }
-    }
-
-    pub fn offset(&'a mut self, offset: i64) -> &'a mut GetUpdates {
-        self.offset = Some(offset);
-        self
-    }
-
-    pub fn limit(&'a mut self, limit: i64) -> &'a mut GetUpdates {
-        self.limit = Some(limit);
-        self
-    }
-
-    pub fn timeout(&'a mut self, timeout: i64) -> &'a mut GetUpdates {
-        self.timeout = Some(timeout);
-        self
-    }
-
-    pub fn allowed_updates(&'a mut self, allowed_updates: &[&'a str]) -> &'a mut GetUpdates {
-        self.allowed_updates = Some(allowed_updates.to_vec());
-        self
-    }
-
-    pub fn send(&self, bot: &super::BotApi) -> Result<Vec<types::Update>, super::BotError> {
-        let url = bot.base_url + "getUpdates";
-        let res = super::request(bot.client.get(&url));
-        match res {
-            Ok(val) => Ok(serde_json::value::from_value(val).unwrap()),
-            Err(e) => Err(e),
-        }
-    } // Find a better way to send. Maybe to intialize, pass bot token. 
+pub struct SetWebhook<'a> {
+    pub url: &'a str,
+    pub certificate: Option<&'a str>,
+    pub max_connections: Option<i64>,
+    pub allowed_updates: Option<Vec<&'a str>>,
 }
 
-
-struct SetWebhook<'a> {
-    url: &'a str,
-    certificate: Option<&'a str>,
-    max_connections: Option<i64>,
-    allowed_updates: Option<Vec<&'a str>>,
+pub struct SendMessage<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub text: &'a str,
+    pub parse_mode: Option<&'a str>,
+    pub disable_web_page_preview: Option<bool>,
+    pub disable_notification: Option<bool>,
+    pub reply_to_message_id: Option<i64>,
+    pub reply_markup: Option<types::ReplyMarkup>,
 }
 
-struct SendMessage<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    text: &'a str,
-    parse_mode: Option<&'a str>,
-    disable_web_page_preview: Option<bool>,
-    disable_notification: Option<bool>,
-    reply_to_message_id: Option<i64>,
-    reply_markup: Option<types::ReplyMarkup>,
+pub struct ForwardMessage<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub from_chat_id: Option<i64>,
+    pub from_chat_username: Option<&'a str>,
+    pub disable_notification: Option<bool>,
+    pub message_id: i64,
 }
 
-struct ForwardMessage<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    from_chat_id: Option<i64>,
-    from_chat_username: Option<&'a str>,
-    disable_notification: Option<bool>,
-    message_id: i64,
+pub struct SendPhoto<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub photo: Option<&'a str>,
+    pub file_id: Option<&'a str>,
+    pub caption: Option<&'a str>,
+    pub disable_notification: Option<bool>,
+    pub reply_to_message_id: Option<i64>,
+    pub reply_markup: Option<types::ReplyMarkup>,
 }
 
-struct SendPhoto<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    photo: Option<&'a str>,
-    file_id: Option<&'a str>,
-    caption: Option<&'a str>,
-    disable_notification: Option<bool>,
-    reply_to_message_id: Option<i64>,
-    reply_markup: Option<types::ReplyMarkup>,
+pub struct SendAudio<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub audio: Option<&'a str>,
+    pub file_id: Option<&'a str>,
+    pub caption: Option<&'a str>,
+    pub duration: Option<i64>,
+    pub performer: Option<&'a str>,
+    pub title: Option<&'a str>,
+    pub disable_notification: Option<bool>,
+    pub reply_to_message_id: Option<i64>,
+    pub reply_markup: Option<types::ReplyMarkup>,
 }
 
-struct SendAudio<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    audio: Option<&'a str>,
-    file_id: Option<&'a str>,
-    caption: Option<&'a str>,
-    duration: Option<i64>,
-    performer: Option<&'a str>,
-    title: Option<&'a str>,
-    disable_notification: Option<bool>,
-    reply_to_message_id: Option<i64>,
-    reply_markup: Option<types::ReplyMarkup>,
+pub struct SendDocument<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub document: Option<&'a str>,
+    pub file_id: Option<&'a str>,
+    pub caption: Option<&'a str>,
+    pub disable_notification: Option<bool>,
+    pub reply_to_message_id: Option<i64>,
+    pub reply_markup: Option<types::ReplyMarkup>,
 }
 
-struct SendDocument<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    document: Option<&'a str>,
-    file_id: Option<&'a str>,
-    caption: Option<&'a str>,
-    disable_notification: Option<bool>,
-    reply_to_message_id: Option<i64>,
-    reply_markup: Option<types::ReplyMarkup>,
+pub struct SendSticker<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub sticker: Option<&'a str>,
+    pub file_id: Option<&'a str>,
+    pub disable_notification: Option<bool>,
+    pub reply_to_message_id: Option<i64>,
+    pub reply_markup: Option<types::ReplyMarkup>,
 }
 
-struct SendSticker<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    sticker: Option<&'a str>,
-    file_id: Option<&'a str>,
-    disable_notification: Option<bool>,
-    reply_to_message_id: Option<i64>,
-    reply_markup: Option<types::ReplyMarkup>,
+pub struct SendVideo<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub video: Option<&'a str>,
+    pub file_id: Option<&'a str>,
+    pub duration: Option<i64>,
+    pub width: Option<i64>,
+    pub height: Option<i64>,
+    pub caption: Option<&'a str>,
+    pub disable_notification: Option<bool>,
+    pub reply_to_message_id: Option<i64>,
+    pub reply_markup: Option<types::ReplyMarkup>,
 }
 
-struct SendVideo<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    video: Option<&'a str>,
-    file_id: Option<&'a str>,
-    duration: Option<i64>,
-    width: Option<i64>,
-    height: Option<i64>,
-    caption: Option<&'a str>,
-    disable_notification: Option<bool>,
-    reply_to_message_id: Option<i64>,
-    reply_markup: Option<types::ReplyMarkup>,
+pub struct SendVoice<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub voice: Option<&'a str>,
+    pub file_id: Option<&'a str>,
+    pub caption: Option<&'a str>,
+    pub duration: Option<i64>,
+    pub disable_notification: Option<bool>,
+    pub reply_to_message_id: Option<i64>,
+    pub reply_markup: Option<types::ReplyMarkup>,
 }
 
-struct SendVoice<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    voice: Option<&'a str>,
-    file_id: Option<&'a str>,
-    caption: Option<&'a str>,
-    duration: Option<i64>,
-    disable_notification: Option<bool>,
-    reply_to_message_id: Option<i64>,
-    reply_markup: Option<types::ReplyMarkup>,
+pub struct SendLocation<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub latitude: f64,
+    pub longitude: f64,
+    pub disable_notification: Option<bool>,
+    pub reply_to_message_id: Option<i64>,
+    pub reply_markup: Option<types::ReplyMarkup>,
 }
 
-struct SendLocation<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    latitude: f64,
-    longitude: f64,
-    disable_notification: Option<bool>,
-    reply_to_message_id: Option<i64>,
-    reply_markup: Option<types::ReplyMarkup>,
+pub struct SendVenue<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub latitude: f64,
+    pub longitude: f64,
+    pub title: &'a str,
+    pub address: &'a str,
+    pub foursquare_id: Option<&'a str>,
+    pub disable_notification: Option<bool>,
+    pub reply_to_message_id: Option<i64>,
+    pub reply_markup: Option<types::ReplyMarkup>,
 }
 
-struct SendVenue<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    latitude: f64,
-    longitude: f64,
-    title: &'a str,
-    address: &'a str,
-    foursquare_id: Option<&'a str>,
-    disable_notification: Option<bool>,
-    reply_to_message_id: Option<i64>,
-    reply_markup: Option<types::ReplyMarkup>,
+pub struct SendContact<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub phone_number: &'a str,
+    pub first_name: &'a str,
+    pub last_name: Option<&'a str>,
+    pub disable_notification: Option<bool>,
+    pub reply_to_message_id: Option<i64>,
+    pub reply_markup: Option<types::ReplyMarkup>,
 }
 
-struct SendContact<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    phone_number: &'a str,
-    first_name: &'a str,
-    last_name: Option<&'a str>,
-    disable_notification: Option<bool>,
-    reply_to_message_id: Option<i64>,
-    reply_markup: Option<types::ReplyMarkup>,
+pub struct GetUserProfilePhotos {
+    pub user_id: i64,
+    pub offset: Option<i64>,
+    pub limit: Option<i64>,
 }
 
-struct GetUserProfilePhotos {
-    user_id: i64,
-    offset: Option<i64>,
-    limit: Option<i64>,
+pub struct GetFile<'a> {
+    pub file_id: &'a str,
 }
 
-struct GetFile<'a> {
-    file_id: &'a str,
+pub struct KickChatMember<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub user_id: i64,
 }
 
-struct KickChatMember<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    user_id: i64,
+pub struct LeaveChat<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
 }
 
-struct LeaveChat<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
+pub struct UnbanChatMember<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub user_id: i64,
 }
 
-struct UnbanChatMember<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    user_id: i64,
+pub struct GetChat<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
 }
 
-struct GetChat<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
+pub struct GetChatAdministrators<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
 }
 
-struct GetChatAdministrators<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
+pub struct GetChatMembersCount<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
 }
 
-struct GetChatMembersCount<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
+pub struct GetChatMember<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub user_id: i64,
 }
 
-struct GetChatMember<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    user_id: i64,
+pub struct AnswerCallbackQuery<'a> {
+    pub callback_query_id: &'a str,
+    pub text: Option<&'a str>,
+    pub show_alert: Option<bool>,
+    pub url: Option<&'a str>,
+    pub cache_time: Option<i64>,
 }
 
-struct AnswerCallbackQuery<'a> {
-    callback_query_id: &'a str,
-    text: Option<&'a str>,
-    show_alert: Option<bool>,
-    url: Option<&'a str>,
-    cache_time: Option<i64>,
+pub struct EditMessageText<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub message_id: Option<i64>,
+    pub inline_message_id: Option<&'a str>,
+    pub text: &'a str,
+    pub parse_mode: Option<&'a str>,
+    pub disable_web_page_preview: Option<bool>,
+    pub reply_markup: Option<types::ReplyMarkup>, // InlineKeyboardMarkup
 }
 
-struct EditMessageText<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    message_id: Option<i64>,
-    inline_message_id: Option<&'a str>,
-    text: &'a str,
-    parse_mode: Option<&'a str>,
-    disable_web_page_preview: Option<bool>,
-    reply_markup: Option<types::ReplyMarkup>, // InlineKeyboardMarkup
+pub struct EditMessageCaption<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub message_id: Option<i64>,
+    pub inline_message_id: Option<&'a str>,
+    pub caption: Option<&'a str>,
+    pub reply_markup: Option<types::ReplyMarkup>, // InlineKeyboardMarkup
 }
 
-struct EditMessageCaption<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    message_id: Option<i64>,
-    inline_message_id: Option<&'a str>,
-    caption: Option<&'a str>,
-    reply_markup: Option<types::ReplyMarkup>, // InlineKeyboardMarkup
+pub struct EditMessageReplyMarkup<'a> {
+    pub chat_id: Option<i64>,
+    pub chat_username: Option<&'a str>,
+    pub message_id: Option<i64>,
+    pub inline_message_id: Option<&'a str>,
+    pub reply_markup: Option<types::ReplyMarkup>, // InlineKeyboardMarkup
 }
 
-struct EditMessageReplyMarkup<'a> {
-    chat_id: Option<i64>,
-    chat_username: Option<&'a str>,
-    message_id: Option<i64>,
-    inline_message_id: Option<&'a str>,
-    reply_markup: Option<types::ReplyMarkup>, // InlineKeyboardMarkup
+pub struct AnswerInlineQuery<'a> {
+    pub inline_query_id: &'a str,
+    pub results: Vec<types::InlineQueryResult>,
+    pub cache_time: Option<i64>,
+    pub is_personal: Option<bool>,
+    pub next_offset: Option<&'a str>,
+    pub switch_pm_text: Option<&'a str>,
+    pub switch_pm_parameter: Option<&'a str>,
 }
 
-struct AnswerInlineQuery<'a> {
-    inline_query_id: &'a str,
-    results: Vec<types::InlineQueryResult>,
-    cache_time: Option<i64>,
-    is_personal: Option<bool>,
-    next_offset: Option<&'a str>,
-    switch_pm_text: Option<&'a str>,
-    switch_pm_parameter: Option<&'a str>,
+pub struct SendGame<'a> {
+    pub chat_id: i64,
+    pub game_short_name: &'a str,
+    pub disable_notification: Option<bool>,
+    pub reply_to_message_id: Option<i64>,
+    pub reply_markup: Option<types::ReplyMarkup>, // InlineKeyboardMarkup
 }
 
-struct SendGame<'a> {
-    chat_id: i64,
-    game_short_name: &'a str,
-    disable_notification: Option<bool>,
-    reply_to_message_id: Option<i64>,
-    reply_markup: Option<types::ReplyMarkup>, // InlineKeyboardMarkup
+pub struct GetGameScore<'a> {
+    pub user_id: i64,
+    pub score: i64,
+    pub force: Option<bool>,
+    pub disable_edit_message: Option<bool>,
+    pub chat_id: Option<i64>,
+    pub message_id: Option<i64>,
+    pub inline_message_id: Option<&'a str>,
 }
 
-struct GetGameScore<'a> {
-    user_id: i64,
-    score: i64,
-    force: Option<bool>,
-    disable_edit_message: Option<bool>,
-    chat_id: Option<i64>,
-    message_id: Option<i64>,
-    inline_message_id: Option<&'a str>,
-}
-
-struct GetGameHighScores<'a> {
-    user_id: i64,
-    chat_id: Option<i64>,
-    message_id: Option<i64>,
-    inline_message_id: Option<&'a str>,
+pub struct GetGameHighScores<'a> {
+    pub user_id: i64,
+    pub chat_id: Option<i64>,
+    pub message_id: Option<i64>,
+    pub inline_message_id: Option<&'a str>,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use hyper::client::Request;
 
 use hyper::client::RequestBuilder;
 use hyper::Client;
+use hyper::Url;
 
 use multipart::client::Multipart;
 
@@ -81,24 +82,131 @@ fn request(rb: RequestBuilder) -> Result<Value, BotError> {
     }
 }
 
-#[derive(Debug, Clone)]
+fn request_multipart(multi: multipart::client::Multipart<hyper::client::Request<hyper::net::Streaming>>) -> Result<Value, BotError> {
+    match multi.send() {
+        Ok(mut res) => {
+            let mut body = String::new();
+            res.read_to_string(&mut body).unwrap();
+            let got_me: ApiResult = serde_json::from_str(&body).unwrap();
+            if let Some(val) = got_me.result {
+                Ok(val)
+            } else {
+                Err(BotError::Api{
+                    error_code: got_me.error_code.unwrap(),
+                    description: got_me.description.unwrap(),
+                    parameters: got_me.parameters
+                })
+            }
+        },
+        Err(e) => {
+            Err(BotError::Http(e))
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct BotApi {
-    base_url: String,
+    base_url: Url,
     client: Client,
 }
 
 impl BotApi {
     pub fn new(bot_token: &str) -> BotApi {
         BotApi {
-            base_url: format!("https://api.telegram.org/bot{}/{{}}", bot_token),
+            base_url: format!("https://api.telegram.org/bot{}/", bot_token).parse().unwrap(),
             client: Client::new(),
         }
     }
 
     pub fn get_me(self) -> Result<User, BotError> {
-        let url = self.base_url + "getMe";
-        let res = request(self.client.get(&url));
+        let url = self.base_url.join("getMe").unwrap();
+        let res = request(self.client.get(url));
         match res {
+            Ok(val) => Ok(serde_json::value::from_value(val).unwrap()),
+            Err(e) => Err(e),
+        }
+    }
+
+    pub fn get_updates(&self, params: &args::GetUpdates) -> Result<Vec<Update>, BotError> {
+        let url = self.base_url.join("getUpdates").unwrap();
+
+        // All these unwraps make me uncomfortable, someone is going to yell
+        // at me to get some good error handling. And they would be right
+        // but I'm really lazy and I don't want to deal with this. I should
+        // though because it's good practice and no real software should ever
+        // just throw errors around like they don't matter.
+
+        // Manage your errors
+
+        let req = Request::new(Method::Post, url).unwrap();
+
+        let mut multi = Multipart::from_request(req).unwrap();
+
+        if let Some(offset) = params.offset {
+            multi.write_text("offset", offset.to_string()).unwrap();
+        }   
+        
+        if let Some(limit) = params.limit {
+            multi.write_text("limit", limit.to_string()).unwrap();
+        }   
+        
+        if let Some(timeout) = params.timeout {
+            multi.write_text("timeout", timeout.to_string()).unwrap();
+        }   
+        
+        // if let Some(allowed_updates) = params.allowed_updates {
+        //     for allowed_update in allowed_updates {
+        //         multi.write_text("allowed_updates[]", allowed_update).unwrap();
+        //     }
+        // } // Cannot move out of borrowed context, :\
+
+        match request_multipart(multi) {
+            Ok(val) => Ok(serde_json::value::from_value(val).unwrap()),
+            Err(e) => Err(e),
+        }
+    }
+
+    
+    pub fn send_message(&self, params: &args::SendMessage) -> Result<Message, BotError> {
+        let url = self.base_url.join("sendMessage").unwrap();
+        let req = Request::new(Method::Post, url).unwrap();
+        let mut multi = Multipart::from_request(req).unwrap();
+
+        if let Some(chat_id) = params.chat_id {
+            multi.write_text("chat_id", chat_id.to_string()).unwrap();
+        } else
+        if let Some(chat_username) = params.chat_username {
+            multi.write_text("chat_id", chat_username).unwrap();
+        }
+
+        multi.write_text("text", params.text).unwrap();
+
+        if let Some(parse_mode) = params.parse_mode {
+            multi.write_text("parse_mode", parse_mode).unwrap();
+        }
+
+        if let Some(disable_web_page_preview) = params.disable_web_page_preview {
+            multi.write_text("disable_web_page_preview", disable_web_page_preview.to_string()).unwrap();
+        }
+
+        if let Some(disable_notification) = params.disable_notification {
+            multi.write_text("disable_notification", disable_notification.to_string()).unwrap();
+        }
+
+        if let Some(reply_to_message_id) = params.reply_to_message_id {
+            multi.write_text("reply_to_message_id", reply_to_message_id.to_string()).unwrap();
+        }
+
+        // if let Some(reply_markup) = params.reply_markup {
+        //     multi.write_text("reply_markup", reply_markup).unwrap();
+        // }
+
+        // TODO create macro or just a function that will take a serde_json::Value
+        // and multi.write_text correctly all of its stuff. then some function for
+        // arrays that is essentially just a loop
+
+
+        match request_multipart(multi) {
             Ok(val) => Ok(serde_json::value::from_value(val).unwrap()),
             Err(e) => Err(e),
         }
@@ -110,10 +218,42 @@ impl BotApi {
 #[cfg(test)]
 mod tests { // These aren't going to be the actual tests, just a place for me to easily test things as I go along
     use super::*;
+    extern crate serde_json;
     #[test]
     fn it_works() {
         let token = "";
         let bot = BotApi::new(token);
-        println!("{:?}", bot.get_me());
+        
+        let mut offset = 0;
+        
+        for _ in 0..3 {
+            let updates = bot.get_updates(&args::GetUpdates {
+                                            offset: Some(offset),
+                                            limit: None,
+                                            timeout: Some(600),
+                                            allowed_updates: None,
+                                        }).unwrap();
+
+            for update in updates {
+                offset = update.update_id;
+
+                if let Some(message) = update.message {
+                    let from = message.from.unwrap();
+                    let text = format!("Hi {}[{}]!", from.first_name,
+                                                     from.id);
+
+                    bot.send_message(&args::SendMessage {
+                                        chat_id: Some(message.chat.id),
+                                        chat_username: None,
+                                        text: &text,
+                                        parse_mode: Some("Markdown"),
+                                        disable_web_page_preview: None,
+                                        disable_notification: None,
+                                        reply_to_message_id: Some(message.message_id),
+                                        reply_markup: None,
+                                    }).unwrap();
+                }
+            }
+        }
     }
 }
